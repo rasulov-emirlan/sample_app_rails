ef25085f469e767213cbc61447321b1b935e1e1c
Emirlan Rasulov <rasulov.emirlan@gmail.com>
2022-06-16 11:15:13 +0600

    finished working on user model

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,11 +2,11 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
-  # test "should get home" do
-  #   get home_path
-  #   assert_response :success
-  #   assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
-  # end
+  test "should get home" do
+    get root_path
+    assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
+  end
 
   test "should get help" do
     get help_path
