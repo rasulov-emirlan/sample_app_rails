8b7b19339eafe272cc035c071634cfd0fa5f1bd8
Emirlan Rasulov <rasulov.emirlan@gmail.com>
2022-06-22 18:09:52 +0600

    started working on chapter 13

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -1,14 +1,10 @@
 require 'test_helper'
 
-class PasswordResetsTest < ActionDispatch::IntegrationTest
-  def setup
-    ActionMailer::Base.deliveries.clear
-  end
-
-  test 'a' do
-    assert true
-  end
-end
+# class PasswordResets < ActionDispatch::IntegrationTest
+#   def setup
+#     ActionMailer::Base.deliveries.clear
+#   end
+# end
 
 # class ForgotPasswordFormTest < PasswordResets
 #   test 'password reset path' do
@@ -18,12 +14,13 @@ end
 #     # assert_select 'input[name=?]', 'password_reset[email]'
 #   end
 
-#   # test 'reset path with invalid email' do
-#   #   post password_resets_path, params: { password_reset: { email: '' } }
-#   #   assert_response :unprocessable_entity
-#   #   assert_not flash.empty?
-#   #   assert_template 'password_resets/new'
-#   # end
+#   test 'reset path with invalid email' do
+#     assert true
+#     #   post password_resets_path, params: { password_reset: { email: '' } }
+#     #   assert_response :unprocessable_entity
+#     #   assert_not flash.empty?
+#     #   assert_template 'password_resets/new'
+#   end
 # end
 
 # class PasswordResetForm < PasswordResets
