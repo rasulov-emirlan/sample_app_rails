f1fe91ad12a56bae6bcb2d663bdf83bb6b589bf4
Emirlan Rasulov <rasulov.emirlan@gmail.com>
2022-06-24 11:30:23 +0600

    feature: completed following system

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,16 @@ Rails.application.routes.draw do
   delete '/logout',    to: 'sessions#destroy'
   delete '/users/:id', to: 'users#destroy'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: %i[new create edit update]
   resources :microposts,          only: %i[create destroy]
+  resources :relationships,       only: %i[create destroy]
+
   get '/microposts', to: 'static_pages#home'
 end
